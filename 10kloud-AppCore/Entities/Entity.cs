0f2a3d249aae8f2a2ae2a74224f4178bc0000c5f
Bolzan Luca <bolzanluca01@gmail.com>
2021-05-13 09:37:53 +0200

    commit della cartella corretta di c#

---
+++ 10kloud-AppCore/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10kloud_AppCore.Entities
+{
+    public abstract class Entity<T>
+    {
+        
+        public T Id { get; set; }
+    }
+}
