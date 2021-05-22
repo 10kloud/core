a8e1d202dfc421c69256b5cc6000254b9ed8ec9c
BolzanLuca <83642308+BolzanLuca@users.noreply.github.com>
2021-05-22 14:01:59 +0200

    implementazione metodi http per accesso alle api

---
+++ 10kloud-AppCore/SilosDTOs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10kloud_AppCore
+{
+    public class SilosDTOs
+    {
+
+
+        public Silos[] Property1 { get; set; }
+
+
+        public class Silos
+        {
+            public string time { get; set; }
+            public string sensor_id { get; set; }
+            public float pressureInternal { get; set; }
+            public int level { get; set; }
+            public int tempExternal { get; set; }
+            public float humidityExternal { get; set; }
+            public string zone { get; set; }
+        }
+
+    }
+}
---
+++ 10kloud-infrastructure/ApiConnection.cs
@@ -0,0 +1,43 @@
+﻿using _10kloud_AppCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace _10kloud_infrastructure
+{
+    class ApiConnection
+    {
+        static async Task<string> GetApiData(string url)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                var client = new HttpClient();
+                HttpResponseMessage ApiResponse = await client.GetAsync(url);
+                ApiResponse.EnsureSuccessStatusCode();
+                string responseBody = await ApiResponse.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+
+                return null;
+            }
+
+            string datainizconv= "%272021-05-19%2008:55:02.509000000%27";
+            string datafineconv;
+            // formato data %272021-05-19%2008:55:02.509000000%27
+            string ApiUrl = await GetApiData("https://3jea5u3n72.execute-api.eu-west-1.amazonaws.com/silos?start=" + datainizconv + "&limit=60");
+
+            SilosDTOs response = JsonSerializer.Deserialize<SilosDTOs>(url);
+
+
+        }
+    }
+}
